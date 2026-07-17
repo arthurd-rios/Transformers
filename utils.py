5101fa33c2653e66b0413046f73bb65091f1d52f
Arthur Domingues Rios <arthurd.rios10@gmail.com>
2026-07-17 17:57:10 -0300

    add matrix_multiply

--- utils.py
+++ utils.py
@@ -1,5 +1,33 @@
-def matrix_multiply():
-    pass
+def matrix_multiply(matrix1, matrix2):
+
+    # Get the dimensions of both matrices
+
+    num_rows1 = len(matrix1)
+    num_cols1 = len(matrix1[0])
+
+    num_rows2 = len(matrix2)
+    num_cols2 = len(matrix2[0])
+
+    # Check if the number of columns in the first matrix equals the number of rows in the second
+
+    if num_cols1 != num_rows2:
+        return
+
+    # Create the result matrix
+
+    matrix = [[0 for _ in range(num_cols2)] for _ in range(num_rows1)]
+
+    # Multiply Logical
+
+    for i in range(num_rows1):
+
+        for j in range (num_cols2):
+
+            for k in range (num_cols1):
+
+                matrix[i][j] = matrix[i][j] + matrix1[i][k]*matrix2[k][j] 
+
+    return matrix
 
 def softmax():
     pass
