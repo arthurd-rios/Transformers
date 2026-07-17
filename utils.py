30ecc9950501d4480f2c9e4bc3e1b79fd9630363
Arthur Domingues Rios <arthurd.rios10@gmail.com>
2026-07-17 14:18:54 -0300

    add utils.py structure

---
+++ utils.py
@@ -0,0 +1,26 @@
+def matrix_multiply():
+    pass
+
+def softmax():
+    pass
+
+def relu():
+    pass
+
+def positional_encoding():
+    pass
+
+class Linear:
+    pass
+
+class LayerNorm:
+    pass
+
+class MultiHeadAttention:
+    pass
+
+class FeedForward:
+    pass
+
+class AddNorm:
+    pass
