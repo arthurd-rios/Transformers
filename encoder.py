6110c997cc4f7159167c5df1615ca16a97ce712d
Arthur Domingues Rios <arthurd.rios10@gmail.com>
2026-07-21 22:30:00 -0300

    add encoder

---
+++ encoder.py
@@ -0,0 +1,32 @@
+import utils
+
+class Encoder:
+
+    def __init__(self, dmodel):
+        
+        self.dmodel = dmodel
+        self.mha = utils.MultiHeadAttention(dmodel)
+        self.addnorm1 = utils.AddNorm(dmodel)
+        self.feedforward = utils.FeedForward(dmodel)
+        self.addnorm2 = utils.AddNorm(dmodel)
+
+    def create_encoder(self):
+        
+        self.mha.create_layer()
+        self.addnorm1.create_layer()
+        self.feedforward.create_layer()
+        self.addnorm2.create_layer()
+
+    def process_encoder(self, words):
+        
+        attention_matrix = self.mha.process_layer(words)
+
+        addnorm_matrix1 = self.addnorm1.process_layer(attention_matrix, words)
+
+        feedforward_matrix = self.feedforward.process_layer(addnorm_matrix1)
+
+        addnorm_matrix2 = self.addnorm2.process_layer(self.feedforward, addnorm_matrix1)
+
+        return addnorm_matrix2
+
+        
